61aad6a6cb1e775076018b96ace08337d17cbf7b
Tommy Abraham <binmulindi.abraham@gmail.com>
2022-03-23 07:56:03 +0200

    some updates

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function openLeftMenu() {
+    document.getElementById("sidebar").style.display = "block";
+  }
+  
+  function closeLeftMenu() {
+    document.getElementById("sidebar").style.display = "none";
+  }
+  
+  function openRightMenu() {
+    document.getElementById("rightMenu").style.display = "block";
+  }
+  
+  function closeRightMenu() {
+    document.getElementById("rightMenu").style.display = "none";
+  }
